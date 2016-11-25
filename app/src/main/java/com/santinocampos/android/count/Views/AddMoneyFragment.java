a7af412c809e46143113bcfbd76e385e52a55540
SanCampos <thedrunkengungan@gmail.com>
2016-11-25 14:15:06 +0800

    feat: linked adding money layout file to its java file

--- app/src/main/java/com/santinocampos/android/count/Views/AddMoneyFragment.java
+++ app/src/main/java/com/santinocampos/android/count/Views/AddMoneyFragment.java
@@ -1,7 +1,9 @@
 package com.santinocampos.android.count.Views;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
@@ -28,6 +30,27 @@ public class AddMoneyFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        //View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_add_money);
+       return new AlertDialog.Builder(getActivity())
+                        .setView(createView(savedInstanceState))
+                        .setTitle(R.string.label_dialog_addMoney)
+                        .setPositiveButton(R.string.btn_addMoney_positive, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mDialogListener.addMoney(Double.parseDouble(mAddMoneyEditText.getText().toString()));
+                            }
+                        })
+                        .setNegativeButton(R.string.btn_addMoney_negative, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dismiss();
+                            }
+                        })
+                        .create();
+    }
+
+    private View createView(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_add_money, null);
+        mAddMoneyEditText = (EditText) v.findViewById(R.id.editText_addMoney);
+        return v;
     }
 }
