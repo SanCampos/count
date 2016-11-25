76e3c61c4622fb679013377ff47ce32557e285fe
SanCampos <thedrunkengungan@gmail.com>
2016-11-25 15:24:51 +0800

    feat: made the addmoney dialog box better

--- app/src/main/java/com/santinocampos/android/count/Dialogs/AddMoneyDialog.java
+++ app/src/main/java/com/santinocampos/android/count/Dialogs/AddMoneyDialog.java
@@ -1,11 +1,11 @@
 package com.santinocampos.android.count.Dialogs;
 
 import android.app.Activity;
-import android.app.AlertDialog;
+import android.support.v7.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -16,7 +16,7 @@ import com.santinocampos.android.count.R;
 /**
  * Created by thedr on 11/16/2016.
  */
-public class AddMoneyDialog extends DialogFragment {
+public class AddMoneyDialog extends AppCompatDialogFragment {
 
     private DialogListener mDialogListener;
     private EditText mAddMoneyEditText;
