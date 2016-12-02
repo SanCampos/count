8a1d606eee722a47932cd4f5b779933ec9f996bf
SanCampos <thedrunkengungan@gmail.com>
2016-12-02 20:32:23 +0800

    refactor: remove walletfragment and integrate with main activity

--- app/src/main/java/com/santinocampos/android/count/Controllers/CounterActivity.java
+++ app/src/main/java/com/santinocampos/android/count/Controllers/CounterActivity.java
@@ -2,7 +2,6 @@ package com.santinocampos.android.count.Controllers;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.santinocampos.android.count.Listeners.DialogListener;
 import com.santinocampos.android.count.Models.Accountant;
@@ -19,12 +19,13 @@ import com.santinocampos.android.count.Dialogs.AddItemDialog;
 import com.santinocampos.android.count.Dialogs.AddMoneyDialog;
 import com.santinocampos.android.count.ViewFragments.ChangeFragment;
 import com.santinocampos.android.count.ViewFragments.ItemListFragment;
-import com.santinocampos.android.count.ViewFragments.WalletFragment;
 
 public class CounterActivity extends AppCompatActivity implements DialogListener {
 
     private final static String DIALOG_ADD_ITEM = "DialogAddItem";
     private final static String DIALOG_ADD_MONEY = "DialogAddMoney";
+
+    private Button mWalletButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +37,17 @@ public class CounterActivity extends AppCompatActivity implements DialogListener
         if (savedInstanceState == null) {
             FragmentManager fm = getSupportFragmentManager();
 
-            WalletFragment walletFragment = new WalletFragment();
             ItemListFragment itemListFragment = new ItemListFragment();
             ChangeFragment changeFragment = new ChangeFragment();
 
-            FragmentTransaction ft = fm.beginTransaction();
-            ft.add(R.id.fragment_container_first, walletFragment);
-            ft.add(R.id.fragment_container_second, itemListFragment);
-            ft.add(R.id.fragment_container_third, changeFragment);
-            ft.commit();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container_second, itemListFragment)
+                    .add(R.id.fragment_container_third, changeFragment)
+                    .commit();
         }
+
+        mWalletButton =  (Button) findViewById(R.id.wallet_totalMoney);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_addItem);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,10 +94,8 @@ public class CounterActivity extends AppCompatActivity implements DialogListener
     }
 
     private void updateUI() {
-        WalletFragment wf = (WalletFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container_first);
         ItemListFragment ilf = (ItemListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container_second);
         ChangeFragment cf = (ChangeFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container_third);
-        wf.update();
         ilf.update();
         cf.update();
     }
