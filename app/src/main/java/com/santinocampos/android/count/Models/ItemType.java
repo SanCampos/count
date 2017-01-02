3767cedaf183dacde42e42cc1de49149c98d3b2e
SanCampos <thedrunkengungan@gmail.com>
2017-01-02 22:57:26 +0800

    change  itemtype implementation to string id instead of hardcoded string

--- app/src/main/java/com/santinocampos/android/count/Models/ItemType.java
+++ app/src/main/java/com/santinocampos/android/count/Models/ItemType.java
@@ -7,17 +7,17 @@ import com.santinocampos.android.count.R;
  */
 
 public enum ItemType {
-    GADGETS(R.drawable.ic_gadget, "GADGET"),
-    FOOD_DRINK(R.drawable.ic_food_drink, "FOOD&DRINK"),
-    ENTERTAINMENT(R.drawable.ic_entertainment, "ENTERTAINMENT"),
-    MISC_PAYMENTS(R.drawable.ic_misc_payments, "MISC_PAYMENTS"),
-    NO_TYPE(R.drawable.ic_no_type, "NO_TYPE"),
-    SCHOOL_SUPPLIES(R.drawable.ic_school_supplies, "SCHOOL_SUPPLIES");
+    GADGETS(R.drawable.ic_gadget, R.string.Gadgets),
+    FOOD_DRINK(R.drawable.ic_food_drink, R.string.Food_Drink),
+    ENTERTAINMENT(R.drawable.ic_entertainment, R.string.Entertainment),
+    MISC_PAYMENTS(R.drawable.ic_misc_payments, R.string.Misc_Payment),
+    NO_TYPE(R.drawable.ic_no_type, R.string.No_Type),
+    SCHOOL_SUPPLIES(R.drawable.ic_school_supplies, R.string.School_Supplies);
 
-    private String mTypeName;
+    private int mTypeName;
     private int mImageID;
 
-    ItemType(int imageID, String itemTypeName) {
+    ItemType(int imageID, int itemTypeName) {
         this.mImageID = imageID;
         this.mTypeName = itemTypeName;
     }
@@ -26,7 +26,7 @@ public enum ItemType {
         return mImageID;
     }
 
-    public String getTypeName() {
+    public int getTypeName() {
         return mTypeName;
     }
 }
