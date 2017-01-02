599d549056ee5d9b10bf684314b848d453b46213
SanCampos <thedrunkengungan@gmail.com>
2017-01-02 21:38:13 +0800

    feat: add itemType model

---
+++ app/src/main/java/com/santinocampos/android/count/Models/ItemType.java
@@ -0,0 +1,10 @@
+package com.santinocampos.android.count.Models;
+
+/**
+ * Created by thedr on 1/2/2017.
+ */
+
+public enum ItemType {
+    FOOD_DRINK, ENTERTAINMENT, SCHOOL_SUPPLIES,
+    GADGETS, MISC_PAYMENTS
+}
