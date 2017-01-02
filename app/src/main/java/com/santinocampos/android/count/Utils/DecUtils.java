089008d6b6edce4de1548d29be11d181e3e0eb5f
SanCampos <thedrunkengungan@gmail.com>
2017-01-02 21:19:13 +0800

    refactor: refactor decimalUtils class' cleaning method

--- app/src/main/java/com/santinocampos/android/count/Utils/DecUtils.java
+++ app/src/main/java/com/santinocampos/android/count/Utils/DecUtils.java
@@ -8,6 +8,9 @@ import java.text.DecimalFormat;
 public class DecUtils {
 
     public static String clean(double money) {
-        return money % 1 == 0 ? String.valueOf(Math.round(money)) : new DecimalFormat("0.00").format(money);
+        StringBuilder decimalFormat = new StringBuilder("#,###");
+        if (money % 1 != 0) decimalFormat.append(".00");
+
+        return new DecimalFormat(decimalFormat.toString()).format(money);
     }
 }
