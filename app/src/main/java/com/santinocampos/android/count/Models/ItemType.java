bfa7ccf754cde6886c296803d248fdb7549146aa
SanCampos <thedrunkengungan@gmail.com>
2017-01-02 21:48:53 +0800

    add and integrate images for itemtype enums

--- app/src/main/java/com/santinocampos/android/count/Models/ItemType.java
+++ app/src/main/java/com/santinocampos/android/count/Models/ItemType.java
@@ -1,10 +1,26 @@
 package com.santinocampos.android.count.Models;
 
+import com.santinocampos.android.count.R;
+
 /**
  * Created by thedr on 1/2/2017.
  */
 
 public enum ItemType {
-    FOOD_DRINK, ENTERTAINMENT, SCHOOL_SUPPLIES,
-    GADGETS, MISC_PAYMENTS
+    GADGETS(R.drawable.ic_gadget),
+    FOOD_DRINK(R.drawable.ic_food_drink),
+    ENTERTAINMENT(R.drawable.ic_entertainment),
+    MISC_PAYMENTS(R.drawable.ic_misc_payments),
+    SCHOOL_SUPPLIES(R.drawable.ic_school_supplies);
+
+    private int imageID;
+
+    ItemType(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
 }
